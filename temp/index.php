5ed8182b42161088e1ed4d641a57f58b047886b5
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-08-19 22:51:41 +0530

    php-2

---
+++ temp/index.php
@@ -0,0 +1,16 @@
+<?php
+include("headter.html")?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    this is the home page<br>
+    stuff about your home page
+</body>
+</html>
+<?php
+include("footer.html")?>
